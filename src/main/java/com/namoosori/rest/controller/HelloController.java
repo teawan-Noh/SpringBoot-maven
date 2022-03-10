db5d536f9bed18b9c7f9d4e6e2f0ae9de4fc9255
DESKTOP-QA0M42J\taewan <ntw2100@gmail.com>
2022-03-10 22:13:13 +0900

    test 완료 with postman

--- src/main/java/com/namoosori/rest/controller/HelloController.java
+++ src/main/java/com/namoosori/rest/controller/HelloController.java
@@ -3,9 +3,11 @@ package com.namoosori.rest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,5 +39,15 @@ public class HelloController {
 	@GetMapping("/users")
 	public List<User> findAll(){
 		return userService.findAll();
+	}
+	
+	@PutMapping("/users")
+	public void modify(@RequestBody User newUser) {
+		userService.modify(newUser);
+	}
+	
+	@DeleteMapping("/users/{id}")
+	public void remove(@PathVariable String id) {
+		userService.remove(id);
 	}
 }
